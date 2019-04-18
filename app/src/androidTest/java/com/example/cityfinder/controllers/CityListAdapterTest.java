9052e77923e1b5811198ca75ec9b0d3bdc123439
elmasrya <andrewelmasry@gmail.com>
2019-04-18 01:50:45 -0400

    Updated gradle configurations Added some Tests

---
+++ app/src/androidTest/java/com/example/cityfinder/controllers/CityListAdapterTest.java
@@ -0,0 +1,20 @@
+package com.example.cityfinder.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CityListAdapterTest {
+
+    @Test
+    public void onCreateViewHolder() {
+    }
+
+    @Test
+    public void onBindViewHolder() {
+    }
+
+    @Test
+    public void getItemCount() {
+    }
+}
---
+++ app/src/androidTest/java/com/example/cityfinder/views/CityFinderActivityTest.java
@@ -0,0 +1,32 @@
+package com.example.cityfinder.views;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CityFinderActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void getCityListFromJSON() {
+    }
+
+    @Test
+    public void readJSONFromAsset() {
+    }
+
+    @Test
+    public void sortCities() {
+    }
+
+    @Test
+    public void initializeCityList() {
+    }
+
+    @Test
+    public void createList() {
+    }
+}
